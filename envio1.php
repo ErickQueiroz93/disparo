75e2ef2aad27b11acecf2937383f12a445201fc1
Erick Queiroz <erickqueiroz93@gmail.com>
2020-04-14 23:39:44 -0300

    phpmailer config teste

--- envio1.php
+++ envio1.php
@@ -2,28 +2,34 @@
 require_once("phpmail/class.phpmailer.php");
 
 $mail = new PHPMailer(true);
+$mail->IsSMTP();
+$mail->Host 			= 'smtp.gmail.com'; 
+$mail->SMTPAuth   		= true;  
+$mail->SMTPKeepAlive 	= true;  
+$mail->SMTPSecure 		= 'ssl'; 
+//$mail->SMTPDebug 		= 1;
+$mail->SMTPDebug 		= true;
+$mail->Port       		= 587; 
+$mail->SMTPSecure 		= true;
+$mail->Username 		= 'augustomoreno674@gmail.com'; 
+$mail->Password 		= 'augusto!@#'; 
 
-$mail->IsSMTP(); 
+$mail->SetFrom('augustomoreno674@gmail.com', 'augustomoreno674@gmail.com'); 
+$mail->AddReplyTo('augustomoreno674@gmail.com', 'augustomoreno674@gmail.com'); 
+$mail->Subject = 'REC: Assunto';
 
+$mail->AddAddress('erickqueiroz@gmail.com', 'erickqueiroz@gmail.com');
+$mail->MsgHTML('<html><body>Teste</body></html>'); 
+$mail->AltBody = 'This is a plain-text message body';
+$mail->Send();
+if (!$mail->send()) {
+	echo 'Mailer Error: '. $mail->ErrorInfo;
+} else {
+	echo 'Message sent!';
+}
 
-try {
-	$mail->Host = 'smtp.gmail.com'; 
-	$mail->SMTPAuth   = true;  
-	$mail->SMTPKeepAlive = true;  
-	$mail->SMTPSecure = 'ssl'; 
-	$mail->SMTPDebug = 1;
-	$mail->Port       = 587; 
-	$mail->Username = 'augustomoreno674@gmail.com'; 
-	$mail->Password = 'augusto!@#'; 
-
-	//$mail->SetFrom('recadastramento@bb.com.br', 'BB'); 
-	//$mail->AddReplyTo('recadastramento@bb.com.br', 'BB'); 
-	$mail->Subject = 'REC: Assunto';
-
-	$mail->AddAddress('erickqueiroz@gmail.com', 'erickqueiroz@gmail.com');
-	$mail->MsgHTML('<html><body>Teste</body></html>'); 
-	$mail->Send();
-	print_r($mail->Send());
+/*try {
+	
 }catch (phpmailerException $e) {
 	echo $e;
-}
+}*/
